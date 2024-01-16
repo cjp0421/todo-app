1d1937ac8fd410ef9c5f87d2aa3b2cd7bab9aa9f
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-01-16 16:47:00 -0600

    started building out ToDo Summary tests

---
+++ frontend/src/__tests__/ToDoSummary.test.tsx
@@ -0,0 +1,9 @@
+import ToDoSummary from "../components/todoSummary/ToDoSummary";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom"
+
+describe("ToDo Summary", () => {
+    it("renders the ToDoSummary component", () => {
+
+    })
+})
