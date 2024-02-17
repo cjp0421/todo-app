57c8ab088283b5edd97a04f01f2e96ae3974e1ad
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-02-17 14:59:04 -0600

    started building out the backend in go

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/cjp0421/todo-app
 
 go 1.21.4
+
+require github.com/rs/cors v1.10.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/rs/cors"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	//define API handlers here
+
+	corsHandler := cors.Default().Handler(mux)
+
+	http.ListenAndServe(":8000", corsHandler)
+}
